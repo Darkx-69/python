a5d02aaea01b8c3456f43481437ebc621c1ba7ac
Darkx-69 <142841709+Darkx-69@users.noreply.github.com>
2024-12-10 23:58:42 +0530

    yres

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+m=14
+n=5
+print(f"Adittioon of two no's {m} and {n} is:-")
+print(m+n)
+print(f"Subtraction of two no's {m} and {n} is:-")
+print(m-n)
+print(f"Multiplication of two no'sm{m} and {n} is:")
+print(m*n)
+print(f"Modulus of two no's {m} and {n} is:-")
+print(m%n)
+print(f"Division of two no's {m} and {n} is:-")
+print(m/n)
+print(f"Square of two no's {m} and {n} is:-")
+print(f"{m**2} and {n**2}")
