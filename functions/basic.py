2fdd09fcd321b5885d95f2950bfaff8148176326
Darkx-69 <142841709+Darkx-69@users.noreply.github.com>
2024-12-14 02:33:37 +0530

    New

---
+++ functions/basic.py
@@ -0,0 +1,7 @@
+def mean( a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+    
+a=8
+b=9
+mean(a,b)
---
+++ loops/while.py
@@ -0,0 +1,7 @@
+i = 20
+while i > 0:  # Loop until i becomes 0
+    if i % 3 == 0:
+        print(f"{i} is divisible by 3")
+    i -= 1  # Decrement i by 1
+else:
+    print("how are you")
