ca84dbdc47e5842f279b65f99af2f7dc01767670
Darkx-69 <142841709+Darkx-69@users.noreply.github.com>
2024-12-17 01:01:06 +0530

    new

---
+++ tuple/basic.py
@@ -0,0 +1,3 @@
+tup=(1,)
+print(type(tup))
+#tuple is same as list but cannot be changed
---
+++ tuple/methods.py
@@ -0,0 +1,5 @@
+name=("Baviya","Anam","Aman")
+temp=list(name)
+temp.append("Abhaya")
+name=tuple(temp)
+print(name)
