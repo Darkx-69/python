52d99a1717691978a9d3d5fcc3e790be8e182424
Darkx-69 <142841709+Darkx-69@users.noreply.github.com>
2024-12-17 23:11:45 +0530

    new

--- functions/recursion.py
+++ functions/recursion.py
@@ -4,4 +4,12 @@ def factorial ( a):
     else:
         return a*factorial(a-1)
     
+def fabonici(a):
+    if a==0:
+        return 0
+    elif a==1:
+        return 1
+    else:
+        return fabonici(a-1)+fabonici(a-2)
 print(factorial(5))
+print(fabonici(3))
